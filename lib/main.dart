8437b0dee9cd359136326b136913f69ea6e2c736
RhuanLino <rhuanlino309@gmail.com>
2025-05-04 16:38:23 -0300

    ajustando rotas7

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'presentation/pages/auth/verifica_cadastro_screen.dart';
-import 'presentation/pages/auth/verifica_trabalhador_screen.dart';
-import 'presentation/pages/auth/cadastro_screen.dart';
-import 'presentation/pages/auth/cadastro_trabalhador_screen.dart';
-import 'presentation/pages/auth/login_screen.dart';
-import 'presentation/pages/auth/recuperar_email_screen.dart';
-import 'presentation/pages/auth/recuperar_codigo_screen.dart';
-import 'presentation/pages/auth/recuperar_nova_senha_screen.dart';
-import 'presentation/pages/home/home_screen.dart';
-import 'presentation/pages/consultas/consultas_screen.dart';
-import 'presentation/pages/debitos/debitos_screen.dart';
-import 'presentation/pages/debitos/mensalidade_screen.dart';
-import 'presentation/pages/debitos/livraria_screen.dart';
-import 'presentation/pages/components/bottom_nav.dart';
+import 'package:celc_app/presentation/App.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'App Flutter',
+      title: 'CELC App',
       theme: ThemeData(
         primaryColor: Color(0xFF199DFF),
         scaffoldBackgroundColor: Colors.white,
@@ -45,23 +31,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (_) => VerificaCadastroScreen(),
-        '/verifica': (_) => VerificaTrabalhadorScreen(),
-        '/cadastro': (_) => CadastroScreen(),
-        '/cadastro_trab': (_) => CadastroTrabalhadorScreen(),
-        '/login': (_) => LoginScreen(),
-        '/recuperar_email': (_) => RecuperarEmailScreen(),
-        '/recuperar_codigo': (_) => RecuperarCodigoScreen(),
-        '/recuperar_nova': (_) => RecuperarNovaSenhaScreen(),
-        '/nav': (_) => BottomNavController(), // redirecionamento após login
-        '/home': (_) => HomeScreen(),
-        '/consultas': (_) => ConsultasScreen(),
-        '/debitos': (_) => DebitosScreen(),
-        '/mensalidade': (_) => MensalidadeScreen(),
-        '/livraria': (_) => LivrariaScreen(),
-      },
+      home: App(),
     );
   }
 }
