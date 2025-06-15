b74c65bfa5b73b7254624702af3699773930d087
RhuanLino <rhuanlino309@gmail.com>
2025-06-15 20:08:59 -0300

    fix | ajustando tela de login com logo e campos

--- lib/presentation/pages/auth/login_screen.dart
+++ lib/presentation/pages/auth/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:celc_app/core/services/auth_requests.dart';
 import 'package:celc_app/presentation/pages/home/home_screen.dart';
+import 'package:celc_app/presentation/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -20,17 +22,17 @@ class _LoginScreenState extends State<LoginScreen> {
     if (email.isEmpty || senha.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Por favor, preencha todos os campos')),
-      );  
+      );
       return;
     }
-  
+
     final response = await login(email, senha);
 
     if (response.containsKey('error')) {
       // Se houver erro no login
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(response['error'])),
-      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(response['error'])));
     } else {
       // Sucesso no login, redirecionando para a tela principal
       Navigator.pushReplacement(
@@ -40,7 +42,6 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,49 +50,89 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.star, size: 100, color: Color(0xFF199DFF)),
             Text(
               'Bem-vindo!',
               style: TextStyle(
-                fontSize: 26,
+                fontSize: 40,
                 color: Color(0xFF199DFF),
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
-            Text('Insira seus dados'),
+
             SizedBox(height: 20),
+
+            Image.asset('assets/images/celc_logo.png', width: 200),
+
+            SizedBox(height: 20),
+            Text('Insira seus dados', style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
             TextField(
-              controller: _emailController, 
+              controller: _emailController,
               decoration: InputDecoration(
                 labelText: 'Email',
                 prefixIcon: Icon(Icons.email),
+                floatingLabelStyle: TextStyle(color: Colors.blue),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 15),
+
             TextField(
               controller: _senhaController,
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Senha',
                 prefixIcon: Icon(Icons.lock),
+                floatingLabelStyle: TextStyle(color: Colors.blue),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
+            SizedBox(height: 8),
+            
             Align(
               alignment: Alignment.centerRight,
               child: TextButton(
-                onPressed: () => Navigator.pushNamed(context, '/recuperar_email'),
-                child: Text('Esqueceu sua senha?'),
+                onPressed:
+                    () => Navigator.pushNamed(context, AppRoutes.recuperarEmail),
+                child: Text(
+                  'Esqueceu sua senha?',
+                  style: TextStyle(color: Color(0xFF199DFF)),
+                ),
               ),
             ),
-            SizedBox(height: 12),
+            SizedBox(height: 15),
+
             ElevatedButton(
               onPressed: _login,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color(0xFF199DFF),
-                minimumSize: Size(double.infinity, 48),
+                minimumSize: Size(300, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
-              child: Text('Entrar'),
+              child: Text(
+                'Entrar',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
